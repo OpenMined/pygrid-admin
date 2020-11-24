c38d473bd24036924c29616eeb70410c00992590
Héricles Emanuel <hericles.me@gmail.com>
2020-11-23 22:06:32 -0300

    test omui by using basic components on homepage

--- src/routes/Homepage.js
+++ src/routes/Homepage.js
@@ -1,8 +1,14 @@
-
 import React from "react";
+import { Container, Box, Heading } from "@openmined/omui"
 
 const Homepage = () => (
-    <h1>Homepage</h1>
+  <Container maxW="xl">
+    <Box padding="4" maxW="3xl">
+      <Heading>
+        PyGrid Admin
+        </Heading>
+    </Box>
+  </Container>
 )
 
 export default Homepage;

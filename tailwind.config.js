03b50565846f2d75cfe0b1a48802c83d5ffcfe6b
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-02-14 01:18:06 +0000

    feat(app): set up tailwind and plugins

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
+  ]
+}

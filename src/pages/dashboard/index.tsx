9e0da8be44fd23cf2b76c485b25918bbd4a7ae78
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-02-14 03:13:13 +0000

    add font-light to dashboard subtitle

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,7 +6,7 @@ const Dashboard: React.FunctionComponent = () => (
   <div>
     <section>
       <h1 className="text-4xl text-gray-800">Dashboard</h1>
-      <p className="mt-3 mb-6 text-xl text-gray-400">An overview of your PyGrid Node</p>
+      <p className="mt-3 mb-6 text-xl font-light text-gray-400">An overview of your PyGrid Node</p>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {cardsData.map(card => (
           <Card key={card.title} {...card} />

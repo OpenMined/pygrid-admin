86cb3e4510881ad3d9c15f235c3e7a79f540c9c6
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-02-20 17:54:56 +0000

    feat: minor config adjustments to tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   bail: 1,
-  clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['components/**/*.js', 'pages/**/*.js'],
-  coverageReporters: ['lcov', 'text']
+  coverageReporters: ['lcov', 'text'],
+  resetMocks: true
 }

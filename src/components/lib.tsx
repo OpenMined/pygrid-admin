897e162287d750e1096f540ae7aabd3737465c67
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-02-21 22:57:08 +0000

    feat: add card and two button types to components/lib

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,8 +1,27 @@
-import {FunctionComponent, PropsWithChildren} from 'react'
+import type {FunctionComponent, PropsWithChildren, HTMLAttributes} from 'react'
 import cn from 'classnames'
 
 export const Tag: FunctionComponent<PropsWithChildren<{className?: string}>> = ({className, children}) => (
   <div className={cn('px-2 font-light text-xs text-white bg-blue-500 rounded-sm whitespace-nowrap', className)}>
     #{children}
   </div>
+)
+
+export const Card: FunctionComponent<PropsWithChildren<{className?: string}>> = ({className, children}) => (
+  <div className={cn('w-full p-6 rounded-md shadow-md bg-gray-50', className)}>{children}</div>
+)
+
+export const ButtonGhost: FunctionComponent<{className?: string} & HTMLAttributes<HTMLButtonElement>> = ({
+  className,
+  ...props
+}) => <button className={cn('btn', 'text-red-500 bg-white', className)} {...props} />
+
+export const ButtonAsLink: FunctionComponent<{className?: string} & HTMLAttributes<HTMLButtonElement>> = ({
+  className,
+  ...props
+}) => (
+  <button
+    className={cn('btn', 'h-auto px-0 text-base leading-6 text-blue-400 normal-case bg-transparent', className)}
+    {...props}
+  />
 )

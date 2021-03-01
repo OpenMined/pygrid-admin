b3cf4c3e63dbe07306aaead9643f0c04f404fb98
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-03-01 15:21:42 +0000

    feat: move context providers to a single file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,20 @@
-import type {FunctionComponent} from 'react'
+import type {ReactNode} from 'react'
 import type {AppProps} from 'next/app'
-import {ReactQueryDevtools} from 'react-query/devtools'
-import {QueryCache, QueryClient, QueryClientProvider} from 'react-query'
-
-import Header from '@/components/header'
+import {AppProviders} from '@/context'
+import {getLayout as getAppLayout} from '@/layouts/app'
+import {CheckAuthRoute} from '@/components/auth-route'
 
 import '@/styles/globals.css'
 
-const queryCache = new QueryCache()
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchInterval: 5 * 60 * 1000
-    }
-  },
-  queryCache: queryCache
-})
+type AppPropsWithLayout = {
+  Component: {getLayout: (FunctionComponent) => ReactNode}
+} & AppProps
 
-const Layout: FunctionComponent = ({children}) => (
-  <div className="min-h-screen">
-    <Header />
-    <div className="container mx-auto mt-8 mb-12">{children}</div>
-  </div>
-)
-
-function PyGridAdmin({Component, pageProps}: AppProps) {
+export default function PyGridAdmin({Component, pageProps}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || getAppLayout
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <AppProviders>
+      <CheckAuthRoute>{getLayout(<Component {...pageProps} />)}</CheckAuthRoute>
+    </AppProviders>
   )
 }
-
-export default PyGridAdmin

ab058229ee26f66d3670d1f27c6ac37f78d965c5
Victor <victor.perez.9889@gmail.com>
2021-03-11 12:41:38 +0000

    Refactor dataset card logic according to api payloads

--- src/components/pages/datasets/cards/datasets.tsx
+++ src/components/pages/datasets/cards/datasets.tsx
@@ -1,21 +1,15 @@
 import type {FunctionComponent} from 'react'
 import {Card} from '@/components/lib'
+import {IDataset} from '@/types/datasets'
 
-interface IDatasetCard {
-  title: string
-  description: string
-  tags: Array<string>
-  tensors: number
-}
-
-const DatasetCard: FunctionComponent<IDatasetCard> = ({title, description, tags, tensors}) => (
+const DatasetCard: FunctionComponent<IDataset> = ({name, description, tags, tensors}) => (
   <Card className="space-y-2 cursor-pointer">
-    <h2 className="text-xl text-gray-800 text-semibold">{title}</h2>
+    <h2 className="text-xl text-gray-800 text-semibold">{name}</h2>
     <p className="max-w-lg font-light text-gray-400 xl:max-w-5xl line-clamp-2">{description}</p>
     <div className="flex flex-col justify-between text-sm text-gray-600 md:flex-row">
       <small>{tags.map(tag => `#${tag} `)}</small>
       <small>
-        {tensors} tensor{tensors !== 1 && 's'}
+        {Object.keys(tensors).length} tensor{Object.keys(tensors).length !== 1 && 's'}
       </small>
     </div>
   </Card>

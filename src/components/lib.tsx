ba0415071377bd7af0f50ee5c6b8308bc7416c14
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-04-01 12:32:23 +0100

    feat: component updates

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,6 +1,8 @@
-import type {FunctionComponent, PropsWithChildren, HTMLAttributes} from 'react'
-import {Search} from '@/components/icons/search'
+import type {FunctionComponent, PropsWithChildren, HTMLAttributes, ComponentProps} from 'react'
+import {forwardRef} from 'react'
 import cn from 'classnames'
+import {Search} from '@/components/icons/search'
+import {CloseCircle} from './icons/marks'
 
 export const Tag: FunctionComponent<PropsWithChildren<{className?: string}>> = ({className, children}) => (
   <div className={cn('px-2 font-light text-xs text-white bg-blue-500 rounded-sm whitespace-nowrap', className)}>
@@ -59,3 +61,76 @@ export const SearchBar: FunctionComponent<SearchBarProperties> = ({search, place
     </div>
   )
 }
+
+export const Alert: FunctionComponent<{
+  className?: string
+  error: string
+  description: string
+}> = ({className, error, description}) => (
+  <div className={cn('p-4 rounded-md bg-red-50', className)}>
+    <div className="flex">
+      <div className="flex-shrink-0">
+        <CloseCircle className="w-5 h-5 text-red-500" />
+      </div>
+      <div className="ml-3">
+        <h3 className="text-sm font-medium text-red-800">{error}</h3>
+        {description && <div className="mt-2 text-sm text-red-700">{description}</div>}
+      </div>
+    </div>
+  </div>
+)
+
+export const Input = forwardRef<HTMLInputElement, ComponentProps<'input'> & {label: string}>(function InputField(
+  props,
+  ref
+) {
+  const {name, label} = props
+  return (
+    <div>
+      {label && (
+        <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+          {label}
+        </label>
+      )}
+      <input
+        ref={ref}
+        type="text"
+        name={name}
+        id={name}
+        className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm placeholder-gray-400"
+        {...props}
+      />
+    </div>
+  )
+})
+
+export const Select = forwardRef<
+  HTMLSelectElement,
+  ComponentProps<'select'> & {label: string; options: {value: string | number; label: string}[]}
+>(function SelectField(props, ref) {
+  const {name, label, placeholder, options, ...inputProps} = props
+  return (
+    <div>
+      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <select
+        {...inputProps}
+        id={name}
+        name={name}
+        ref={ref}
+        className="block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md invalid:text-gray-400">
+        {placeholder && (
+          <option value="" disabled hidden>
+            {placeholder}
+          </option>
+        )}
+        {options.map(({value, label}) => (
+          <option key={`option-${value}`} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+})

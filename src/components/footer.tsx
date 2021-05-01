12b7590526000c66d3eaac2b57abcc8fbdc30661
Victor <victor.perez.9889@gmail.com>
2021-05-01 17:38:09 +0100

    Fix style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,10 @@ const Footer: FunctionComponent = () => {
       <nav className="sticky bottom-0">
         <div className="flex flex-row flex-wrap items-center justify-between py-2 shadow bg-gray-50 border">
           <div className="container mx-auto flex flex-row items-center space-x-2">
-            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 animate-pulse fill-current text-green-500">
+            <svg
+              viewBox="0 0 100 100"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-3 w-3 animate-pulse fill-current text-green-500">
               <circle cx="50" cy="50" r="50" />
             </svg>
             <span className="text-xs text-gray-600">Connected to {status?.domainName}</span>

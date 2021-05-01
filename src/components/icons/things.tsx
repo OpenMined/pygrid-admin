e321aaceadbe74406a2d901691c5de61a3944dcb
Victor <victor.perez.9889@gmail.com>
2021-05-01 13:08:14 +0100

    Add Icon Logout

--- src/components/icons/things.tsx
+++ src/components/icons/things.tsx
@@ -16,4 +16,25 @@ const Mail: FunctionComponent<ComponentProps<'svg'>> = ({className}) => {
   )
 }
 
-export {Mail}
+const Logout: FunctionComponent<ComponentProps<'svg'>> = ({className}) => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+      className={cn('inline', className)}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+      />
+    </svg>
+  )
+}
+
+export {Mail, Logout}

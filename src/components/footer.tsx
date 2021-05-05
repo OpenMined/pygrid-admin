aa6ceac956a2ec033203a3ab88fb478de7839df6
Victor <victor.perez.9889@gmail.com>
2021-05-05 18:37:59 +0100

    Moved dot icon to icons folder

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import {useFetch} from '@/utils/query-builder'
 import {PyGridStatus} from '@/types'
 import cn from 'classnames'
 import packageJson from '../../package.json'
+import {Dot} from '@/components/icons/marks'
 
 const Footer: FunctionComponent = () => {
   const {isLoading, error, isError, data: status} = useFetch<PyGridStatus>('/setup/status')
@@ -12,15 +13,12 @@ const Footer: FunctionComponent = () => {
       <nav className="sticky bottom-0">
         <div className="flex flex-row flex-wrap items-center justify-between py-2 bg-gray-50 border">
           <div className="container mx-auto flex flex-row items-center space-x-2">
-            <svg
-              viewBox="0 0 100 100"
-              xmlns="http://www.w3.org/2000/svg"
+            <Dot
               className={cn(
-                'h-3 w-3 animate-pulse fill-current',
+                'h-3 w-3 fill-current',
                 isLoading ? 'text-yellow-500' : isError ? 'text-red-500' : 'text-green-500'
-              )}>
-              <circle cx="50" cy="50" r="50" />
-            </svg>
+              )}
+            />
             {isLoading && <span className="text-xs text-gray-600">Connecting to PyGrid Domain...</span>}
             {isError && <span className="text-xs text-gray-600">PyGrid API unreachable</span>}
             {status && (

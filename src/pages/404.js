8948ceefeb2abe9787ed4a046b8c134d14e59482
Victor Perez <victor.perez.9889@gmail.com>
2021-05-26 17:48:58 +0100

    Refactor 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import {Title, Subtitle, ButtonRound} from '@/components/lib'
+import Link from 'next/link'
 
-const NoMatch = () => <h1>404</h1>
+const NoMatch = () => {
+  return (
+    <article className="flex justify-center items-center">
+      <div className="text-center justify-items-center grid grid-flow-row auto-rows-max space-y-4">
+        <header>
+          <Title>Ooops!</Title>
+          <Subtitle>This page is unknown or does not exist</Subtitle>
+        </header>
+        <Link href="/">
+          <button className="w-full btn">Back to Dashboard</button>
+        </Link>
+      </div>
+    </article>
+  )
+}
 
 export default NoMatch
